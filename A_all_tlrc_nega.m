d7fa1431edb72b40152d6b1471c5684c42a789a7
Zhang Manqi <59470071+manqiac@users.noreply.github.com>
2024-08-01 20:14:43 +0800

    Add files via upload

---
+++ A_all_tlrc_nega.m
@@ -0,0 +1,39 @@
+clc;
+clear all;
+
+dirs.fMRI = '../../data/processed_data/Nega_voxel_processed/tlrc_t/';
+dirs.tlrc = '../../data/transformed_mat/negative12dofsm4_3mm_t/tlrc_t/';
+tlrc_outputpath = fullfile(dirs.tlrc);
+if ~exist(tlrc_outputpath), mkdir(tlrc_outputpath); end
+
+
+all_tlrc = [];
+tlrc_id = [];
+
+for i = 1:60
+    subj1 = sprintf('t%03d', i);
+    fprintf('subject %i \n', i);
+    for j = i+1:60
+        subj2 = sprintf('t%03d', j);
+        nii = load_nii(fullfile(dirs.fMRI,sprintf('%s_%s_tlrc.nii', subj1, subj2)));   
+        if strcmp(subj1, subj2)
+            continue;
+        end
+        data = nii.img;   
+        datasize = size(data);   
+        
+        data = single(reshape(data, [(size(data, 1) * size(data, 2) * size(data, 3)), size(data, 4)]));   % 重塑数据
+%       keptvox = mdata > mcutoff;   
+%       movie_data = data(keptvox, :);
+        subj1_subj2 = cellstr(strcat(subj1, '_', subj2));
+        subj2_subj1 = cellstr(strcat(subj2, '_', subj1));
+        a = [subj1_subj2; subj2_subj1];
+        b = vertcat(data',data');
+        tlrc_id = vertcat(tlrc_id, a);
+        all_tlrc = vertcat(all_tlrc, b);
+    end
+end
+
+all_tlrc1 = all_tlrc(1:1770,:);
+all_tlrc2 = all_tlrc(1771:3540,:);
+save(fullfile(dirs.tlrc, 'ALL_tlrc_nega.mat'),'tlrc_id','all_tlrc1','all_tlrc2');
